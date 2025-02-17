1103c013f5235e1670ceb825b3365579f7804a5f
sunnyju318 <sunnyju318@naver.com>
2025-02-16 23:32:56 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 
+
+document.addEventListener("DOMContentLoaded", 
+  
+  function () {
+  const dropdownLinks = document.querySelectorAll(".dropdown a");
+
+  dropdownLinks.forEach(link => {
+    link.addEventListener("mouseover", function () {
+      link.style.color = "#36454f";
+    });
+
+    link.addEventListener("mouseout", function () {
+      link.style.color = "#708090";
+    });
+  });
+}
+);
