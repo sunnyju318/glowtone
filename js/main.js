ce685911742dea8b266af9c75169db09ba9365fb
mac <mac@macui-MacBookPro.local>
2025-02-16 22:55:09 -0800

    fist file upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { loadHeaderFooter } from "/js/modules/loadHeaderFooter.js";
+import { initDropdownHover } from "/js/modules/dropdown.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadHeaderFooter();
+  initDropdownHover();
+});
---
+++ js/modules/loadHeaderFooter.js
@@ -0,0 +1,22 @@
+
+import { initDropdownHover } from "/js/modules/dropdown.js"; 
+
+export function loadHeaderFooter() {
+  fetch("../../partials/header.html") 
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    });
+
+    fetch("../../partials/nav.html") 
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("nav-placeholder").innerHTML = data;
+      initDropdownHover(); 
+    });
+
+      
+  fetch("../../partials/footer.html") 
+    .then(response => response.text())
+    .then(data => document.getElementById("footer-placeholder").innerHTML = data);
+};
